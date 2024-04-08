3c4d89fd9ab4a0e361ba55e041a8bb81b9fbaff7
Wandy Muñoz <whandy_1018@hotmail.com>
2024-04-08 16:04:49 -0500

    Trabajos clase hasta 04/04/24

---
+++ src/main/java/Clase4/TrabajoClase3.java
@@ -0,0 +1,28 @@
+package Clase4;
+
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TrabajoClase3 {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in); //Se inicializa el scanner
+        List<String> palabras = new ArrayList<>(); //Se crea la lista
+
+        System.out.println("A continuación ingrese 4 palabras");
+        for (int i = 1; i <= 4; i++){
+            System.out.print("Por favor ingrese una palabra: ");
+            String palabra = scanner.next();    //Se toman las palabras ingresadas por el usuario
+            palabras.add(palabra);
+        }
+        for (String datosArreglo:palabras){
+            if (datosArreglo.length()>4){
+                System.out.println(datosArreglo);   //Se muestran las palabras con más de 4 letras
+            }
+        }
+        scanner.close(); //Se finaliza el scanner
+    }
+}
